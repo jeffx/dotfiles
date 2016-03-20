3b28155fa9ed3d07c67b65c457edda06beb0ea29
Jeff Tillotson <jpt@jeffx.com>
2016-03-20 15:22:15 -0400

    Apply a quick setup script to install all the dotfiles

--- setup.sh
+++ setup.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-[[ -d ${HOME}/projects/dotfiles ]] || exit
-cd ${HOME}
-[[ -f ${HOME}/.bash_profile ]] || [[ -h ${HOME}/.bash_profile ]] && rm -f ${HOME}/.bash_profile
-ln -s ${HOME}/projects/dotfiles/bash/bash_profile ${HOME}/.bash_profile
-[[ -f ${HOME}/.bashrc ]] || [[ -h ${HOME}/.bashrc ]] && rm -f ${HOME}/.bashrc
-ln -s ${HOME}/projects/dotfiles/bash/bashrc ${HOME}/.bashrc
-[[ -f ${HOME}/.gitignore ]] || [[ -h ${HOME}/.gitignore ]] && rm -f ${HOME}/.gitignore
-ln -s ${HOME}/projects/dotfiles/git/global-ignore ${HOME}/.gitignore
-[[ -f ${HOME}/.gitconfig ]] || [[ -h ${HOME}/.gitconfig ]] && rm -f ${HOME}/.gitconfig
-ln -s ${HOME}/projects/dotfiles/git/gitconfig ${HOME}/.gitconfig
-[[ -f ${HOME}/.tmux.conf ]] || [[ -h ${HOME}/.tmux.conf ]] && rm -f ${HOME}/.tmux.conf
-ln -s ${HOME}/projects/dotfiles/tmux/tmux.conf ${HOME}/.tmux.conf
+date_stamp=$(date +%j-%H%M)
+source_dir=$(dirname $0)
+source_dir=$(realpath ${source_dir})
+dot_file_list="bash/bashrc
+git/gitconfig
+git/global-ignore
+screen/screenrc
+tmux/tmux.conf
+vim/vimrc"
 
+backup_dir="${HOME}/.dotbackups"
 
-
+for file in ${dot_file_list} ; do
+    bn=$(basename ${file})
+    df="${HOME}/.${bn}"
+    sf="${source_dir}/${file}"
+    echo "bf=${df}"
+    if [ -h ${df} ] ; then
+        rm -f ${df}
+    elif [ -f ${df} ] ; then
+        [ -d ${backup_dir} ] || mkdir ${backup_dir}
+        mv ${df} ${backup_dir}/${bn}.${date_stamp}
+    fi
+    [ -f ${sf} ] || echo "${sf} doesn't exist"
+    ln -s ${sf} ${df}
+done
 
